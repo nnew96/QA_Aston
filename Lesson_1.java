4cdda43d712b706f1d3300ba35d3fdbe925bd8e7
nnew96 <nelli_new96@mail.ru>
2024-09-15 22:38:07 +0300

    /

---
+++ Lesson_1.java
@@ -0,0 +1,13 @@
+public class Lesson_1 {
+    public static void main (String[] args) {
+        printThreeWords();
+    }
+    // 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    // 2
+
+}
